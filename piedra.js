f8fe7cd68afa74597740d6d5c38b02f5e1dcec93
Sam01-maker <205439086+Sam01-maker@users.noreply.github.com>
2025-07-07 15:28:59 -0400

    Crear la funcion playRound

--- piedra.js
+++ piedra.js
@@ -41,4 +41,9 @@ function getHumanChoice () {
 let humanScore = 0;
 let computerScore = 0;
 
-//Logica pra el primer juego
+//Logica para la primera ronda.
+
+function playRound(getComputerChoice, getHumanChoice){
+    
+
+}
