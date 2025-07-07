f48a7e51208005d5e6705bf1fe0af0b67ad7ff85
Sam01-maker <205439086+Sam01-maker@users.noreply.github.com>
2025-07-07 15:01:47 -0400

    Agregar ToUpperCase para las funciones de captura
    Para que no importe si los usuarios ingresan mayusculas o minusculas

--- piedra.js
+++ piedra.js
@@ -11,31 +11,34 @@ function getComputerChoice(){
         choiceComputer= 'Tijera';
     }
 
-    return choiceComputer;
+    return choiceComputer.toUpperCase();
 }
 
 
 //Funcion paara obetener la eleccin del usuario
 function getHumanChoice () {
-    let choiceHuman = prompt('Digita una opcion entre "Piedra", "Papel" o "Tijera": ');
+    let choiceHuman = prompt('Digita una opcion entre "Piedra", "Papel" o "Tijera": ').toUpperCase();
     
-    if (choiceHuman === 'piedra' || choiceHuman ===  'Piedra' || choiceHuman ===  'PIEDRA'){
+    if (choiceHuman === 'PIEDRA'){
         choiceHuman = 'Piedra';
 
-    } else if (choiceHuman === 'papel' || choiceHuman === 'Papel' || choiceHuman === 'PAPEL'){
+    } else if (choiceHuman === 'PAPEL'){
         choiceHuman = 'Papel';
 
-    } else if (choiceHuman === 'tijera' || choiceHuman === 'Tijera' || choiceHuman ===  'TIJERA'){
+    } else if (choiceHuman === 'TIJERA'){
         choiceHuman = 'Tijera';
 
     } else{
         choiceHuman = 'Seleccionar un valor valido';
     }
 
-    return choiceHuman;
+    return choiceHuman.toUpperCase();
 }
+
 
 //Variables de puntuacion de los jugadores.
 
 let humanScore = 0;
 let computerScore = 0;
+
+//Logica pra el primer juego
