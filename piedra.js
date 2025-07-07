7eac213688ea429ecfdfc5c0dfb8545cd050ea55
Sam01-maker <205439086+Sam01-maker@users.noreply.github.com>
2025-07-06 22:32:35 -0400

    Crear funcion getHumanChoice

--- piedra.js
+++ piedra.js
@@ -12,3 +12,18 @@ function getComputerChoice(){
     return choiceComputer;
 }
 
+function getHumanChoice () {
+    let choiceHuman = prompt('Selecciona una opcion entre "Piedra", "Papel" o "Tijeras"');
+    
+    if (choiceHuman === 'piedra' || 'Piedra' || 'PIEDRA'){
+        choiceHuman = 'Piedra';
+    } else if (choiceHuman === 'papel' || 'Papel' || 'PAPEL'){
+        choiceHuman = 'Papel';
+    } else if (choiceHuman === 'tijera' || 'Tijera' || 'TIJERA'){
+        choiceHuman = 'Tijera';
+    } else{
+        choiceHuman = 'Seleccionar un valor valido';
+    }
+}
+
+console.log(getHumanChoice());
