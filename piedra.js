f28bfa59f575c88e427c6bc72d36c6b78c31a149
Sam01-maker <205439086+Sam01-maker@users.noreply.github.com>
2025-07-07 21:23:36 -0400

    Crear la funcion playGame

--- piedra.js
+++ piedra.js
@@ -41,6 +41,7 @@ function getHumanChoice () {
 let humanScore = 0;
 let computerScore = 0;
 
+
 //Logica para la primera ronda.
 
 
@@ -48,11 +49,59 @@ function playRound(choiceHuman, choiceComputer){
     choiceHuman = choiceHuman.toUpperCase();
     choiceComputer = choiceComputer.toUpperCase();
       
+    //codigo condicional con algoritmo del juego 
+
+    if (choiceHuman === choiceComputer){
+        return console.log('Han elejido el mismo objeto, Intenta de nuevo');
+    } else if (choiceHuman === "PIEDRA" && choiceComputer === "TIJERA"){
+        ++humanScore;
+        return console.log(`Felicidades "Piedra gana a Tijera"`);
+        
+    } else if (choiceHuman === "PIEDRA" && choiceComputer === "PAPEL"){
+        ++computerScore;
+        return console.log(`Lo siento "Papel gana a Piedra"`);
+    
+    } else if (choiceHuman === "PAPEL" && choiceComputer === "PIEDRA"){
+        ++humanScore;
+        return console.log(`Felicidades "Papel gana a Piedra"`);
+    
+    } else if (choiceHuman === "PAPEL" && choiceComputer === "TIJERA"){
+        ++computerScore;
+        return console.log(`Lo Siento "Tiejera gana a Papel"`);
+    
+    }else if (choiceHuman === "TIJERA" && choiceComputer === "PAPEL"){
+        ++humanScore;
+        return console.log(`Felicidades "Tiejera gana a Papel"`);
+    
+    } else if (choiceHuman === "TIJERA" && choiceComputer === "PIEDRA"){
+        ++computerScore;
+        return console.log(`Lo siento "Piedra gana a Papel"`);
+    
+    } 
+}
+
+// const  humanSelection = getHumanChoice();
+// const  computerSelection = getComputerChoice();
+
+
+//Funcion para la logica de todo el juego.
+
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        const  humanSelection = getHumanChoice();
+        const  computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        
+    }
+
+    if (humanScore > computerScore){
+        return console.log(`Felicidades Ganaste, obtuviste: ${humanScore} puntos. La computadora ${computerScore}.` );
+    } else if (humanScore === computerScore){
+         return console.log(`Es un Empate, obtuviste: ${humanScore} puntos. La computadora ${computerScore}.` );
+    } else{
+        return console.log(`Lo siento has perdido, obtuviste: ${humanScore} puntos. La computadora ${computerScore}.`);
+    }
     
 
 }
-
-const  humanSelection = getHumanChoice();
-const  computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
+playGame();
